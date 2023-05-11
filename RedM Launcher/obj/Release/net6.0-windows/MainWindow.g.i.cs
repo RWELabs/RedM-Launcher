599458979a6c3d219ee1b7c3f593a854e18f7ae3
Ryan Walpole <ryan.walpole@outlook.com>
2023-05-11 11:24:17 +1000

    Update Build v230502
    Update build 230502 which additionally clears the data\cache folder.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- RedM Launcher/obj/Release/net6.0-windows/MainWindow.g.i.cs
+++ RedM Launcher/obj/Release/net6.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace RedM_Launcher {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RedM Launcher;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RedM Launcher;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
