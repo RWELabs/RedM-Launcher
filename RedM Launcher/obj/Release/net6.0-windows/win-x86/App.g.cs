991d531e012a11e4903a7c41b174375a0adda2fa
Ryan Walpole <ryan.walpole@outlook.com>
2023-06-27 16:07:43 +1000

    Update Release Objects
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- RedM Launcher/obj/Release/net6.0-windows/win-x86/App.g.cs
+++ RedM Launcher/obj/Release/net6.0-windows/win-x86/App.g.cs
@@ -45,7 +45,7 @@ namespace RedM_Launcher {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace RedM_Launcher {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public static void Main() {
             RedM_Launcher.App app = new RedM_Launcher.App();
             app.InitializeComponent();
